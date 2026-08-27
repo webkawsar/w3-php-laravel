cb016d9d258713a2796fdd702cbc6a332a87f9b6
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-08-27 07:36:03 +0600

    practice superglobal, form, get, post, request, include, session, cookie

---
+++ class-8/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Index 
+// Superglobal, Form, GET, POST, Cookie, Session, Include, Require
+
+
+
+
+// include ar require er majhe difference holo include use korle file na pele warning diye
+// porer code execute hoye jay but require use korle error show kore and porer code execute hoy na
+
+
+
+
+
+
+
+
+
+
