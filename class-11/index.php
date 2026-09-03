0b4b59e6ad9b3eccbf5431dc7084ded50e0d525c
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2026-09-03 08:12:36 +0600

    practice sql command

---
+++ class-11/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Index 
+// Insert, Select, Where, Update, Delete, Order By, Limit, CRUD Operation
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class-12/index.php
@@ -0,0 +1,45 @@
+<?php
+
+// Index 
+// Normalization, Primary & Foreign Keys, Relationships, Join
+
+
+// Create Database by SQL
+// CREATE DATABASE school;
+
+// USE school;
+
+
+// Create Table by SQL Command
+// CREATE TABLE students (
+// id INT AUTO_INCREMENT PRIMARY KEY,
+// name VARCHAR(100),
+// email VARCHAR(100),
+// age TINYINT UNSIGNED
+// );
+
+
+// Update table by SQL 
+// ALTER TABLE students
+// ADD COLUMN phone VARCHAR(20) AFTER age;
+
+// ALTER TABLE students
+// ADD COLUMN phone VARCHAR(20),
+// ADD COLUMN address VARCHAR(255);
+
+
+
+
+// Data Insert to Table
+// INSERT INTO students (`name`, `email`, `age`)
+// VALUES
+// ("Kawsar Ahmed", "kawsar@gmail.com", 30),
+// ("Samim Ahmed", "samim@gmail.com", 30);
+
+
+
+
+
+
+
+
